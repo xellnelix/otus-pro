dff25a3a4ec3920fddc5c9ba0429fca22d15e87b
xellnelix <xellnelix@gmail.com>
2024-02-07 20:33:12 +0300

    Copied "Homework" module

---
+++ hw16-io/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "ru.otus.xellnelix"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("ch.qos.logback:logback-classic")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
